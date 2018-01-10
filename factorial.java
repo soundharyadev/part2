51178d80dcd027354d05afe879eed43dea137828
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-01-10 20:07:43 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,20 +1,24 @@
-impport java.io.*;
+import java.io.*;
 import java.util.Scanner;
 public class Fact
 {
 public static void main(String[]s)
 {
-int i fact=1;
+int i, fact=1;
 int n;
 Scanner sc= new Scanner(System.in);
 System.out.println("Enter the number:");
 n=sc.nextInt();
-while(n!==0)
+if(n<0)
+{
+  System.ouot,println("-ve number");
+}
+  else
 {
 for(i=0;i<n;i++)
 {
 fact=fact*i;
-System.lout.println("the factorial of number is: +fact)
+System.out.println("the factorial of number is:" +fact);
 }
 }
 }
