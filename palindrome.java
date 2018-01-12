94e6b0f528096f0180b473d20efcdd99a0acacf7
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-01-12 17:03:13 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String []s)
+    {
+        System.out.println("enter the number:");
+        Scanner r=new Scanner(System.in);
+        int num=r.nextInt();
+        int n =num;
+        int  rev=0,rmd;
+        while(num>0)
+        {
+            rmd=num%10;
+            rev=rev*10+rmd;
+            num=num/10;
+        }
+        if(rev==n){
+            
+            System.out.println(n+ "is a palindrome number!:");
+        
+        }
+        else
+        {
+            System.out.println(n+ "is not a palindrome number!");
+        }
+   
+        
+    }
+}
