a83d659e0264fe2a47041f1509ecd3fd9ae11fe5
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-05 15:59:16 +0530

    Rename char.c to characters.c

---
+++ characters.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZE 100 
+
+
+int main()
+{
+    char str[SIZE];
+    char * s = str;
+    int alphabets, digits, others,a;
+
+    alphabets = digits = others = 0;
+    printf("Enter any string : ");
+    gets(str);
+
+    while(*s)
+    {
+        if((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
+            alphabets++;
+        else if(*s>='0' && *s<='9')
+            digits++;
+        else
+            others++;
+
+        s++;
+        a=alphabets+digits+others;
+    }
+    printf("%d\n",a);
+
+    return 0;
+}
