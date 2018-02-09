23df56bf28a6005b6798bd01d34a34c0920a8add
soundharyadev <30386716+soundharyadev@users.noreply.github.com>
2018-02-09 16:00:31 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+printf("ener the number :");
+scanf("%d",&n);
+if(n==0||n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9)
+{
+	printf("Yes");
+}
+else
+{
+	printf("No");
+}
+}
